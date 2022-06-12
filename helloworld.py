f34112709303f1201dcbed1c60622b81f644ac6c
texasjeeper15 <chrishodges247@gmail.com>
2022-06-12 00:12:34 -0500

    imported openpyxl and sample code to read a value.

--- helloworld.py
+++ helloworld.py
@@ -2,3 +2,9 @@ print ("helloworld")
 print ("line2")
 print ("line3")
 print ("line4")
+import openpyxl
+path = "C:\CommonUser\py_sheet1.xlsx"
+wb_obj = openpyxl.load_workbook(path)
+sheet_obj = wb_obj.active
+cell_obj = sheet_obj.cell(row = 1, column = 1)
+print(cell_obj.value)
